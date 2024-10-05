483be83871581835066418ae9027482009997382
Pass <sap215@miami.edu>
2024-10-05 15:53:43 -0400

    Starting Matchsticks problem

---
+++ Matchsticks/Solution.c
@@ -0,0 +1,27 @@
+//------------------------------------------------------------------
+
+//Includes
+#include <stdio.h>
+#include <stdlib.h>
+#inclued <math.h>
+
+//------------------------------------------------------------------
+
+//Main function
+int main(void) {
+	
+	//Define values for each number
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
